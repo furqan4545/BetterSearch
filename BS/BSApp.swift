77480c6085896773bad97781eea54d762f283497
Furqan Ali <furqan.ali9500@gmail.com>
2026-03-21 20:18:21 +0900

    Initial Commit

---
+++ BS/BSApp.swift
@@ -0,0 +1,17 @@
+//
+//  BSApp.swift
+//  BS
+//
+//  Created by Furqan Ali on 3/21/26.
+//
+
+import SwiftUI
+
+@main
+struct BSApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
